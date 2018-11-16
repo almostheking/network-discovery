06aefea76653bf9267f7bc70a76412e30d0275b4
Joseph Lignelli <36961885+almostheking@users.noreply.github.com>
2018-11-16 11:20:21 -0500

    added comments

--- log_v2.sh
+++ log_v2.sh
@@ -1,3 +1,5 @@
+# This script logs connections and disconnections from a given network by periodically scanning for active devices.
+
 #!/bin/bash
 
 set -o errexit
@@ -9,10 +11,12 @@ readonly FULLDATE=$(date)
 readonly DATESTAMP=$(date | awk '{print $1,$2,$3}')
 readonly WD=/tmp
 
+# Function scans and parses IP range results.
 _scan_and_parse () {
 	sudo nmap -R -sP $IP_RANGE | grep -v "Host" | tr '\n' ' ' | sed 's|Nmap|\nNmap|g' | egrep '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}'  | cut -d " " -f5,8-15 | awk '{gsub("Address: ", "");print}' | sort> $WD/scan.$count.txt;
 }
 
+# This creates a running scan list by comparing the previous loop's scanlist with the current loop's scan results.
 _create_scan_results () {
 	if [ -e $WD/scanlist.txt ]; then
 		comm -13 $WD/scanlist.txt $WD/scan.$count.txt > $WD/unique.txt;
@@ -24,11 +28,13 @@ _create_scan_results () {
 	fi
 }
 
+# Function simply creates lists of devices to add or subtract from the rolling list.
 _addsub_lists () {
 	comm -23 $WD/rolling.txt $WD/scanlist.txt > $WD/sub.txt;
 	comm -13 $WD/rolling.txt $WD/scanlist.txt > $WD/add.txt;
 }
 
+# Function processes the add and sub lists, turning them into log entries and attaching them to the running log file.
 _proc_addsub_lists () {
 	if [[ $(wc -w <$WD/sub.txt) -gt 0 ]]; then
 		awk -v var="~$FULLDATE DISCONNECTED: " '{print var$0}' $WD/sub.txt > $WD/tmp && mv $WD/tmp $WD/sub.txt;
@@ -43,11 +49,13 @@ _proc_addsub_lists () {
 	mv $WD/scanlist.txt $WD/rolling.txt; 
 }
 
+# Function archives the running log file and clears the way for a new one to be made
 _archive_cumulo () {
 	tar -cvf "$(cat cumulative.txt | grep "Log Generated: " | awk '{print $3,$4,$5}')_archive.tar" cumulative.txt;
 	rm -f $WD/rolling.txt cumulative.txt;
 }
 
+# Creates a new running log file based on scanlists results
 _gen_cumulo () {
 	mv $WD/scanlist.txt $WD/rolling.txt;
 	echo -e "Log Generated: $FULLDATE\n" > cumulative.txt;

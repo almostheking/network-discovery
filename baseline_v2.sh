a27bd23b40991a90691122de4f322b61c5d0ff47
Joseph Lignelli <36961885+almostheking@users.noreply.github.com>
2018-11-16 10:56:36 -0500

    added comments

--- baseline_v2.sh
+++ baseline_v2.sh
@@ -1,3 +1,6 @@
+# This script, when given an IP range to scan, will begin to generate an asset whitelist to be used alongside the alarm_scan.sh script.
+# Note that this script ought to be run multiple times over a period of time, and the results need to be analyzed in order to determine a final whitelist.
+
 #!/bin/bash
 
 set -o errexit
@@ -7,16 +10,19 @@ set -o pipefail
 readonly IP_RANGE=$1
 readonly WD=/tmp
 
+# Function scans and parses given IP range.
 _scan_and_parse () {
 	sudo nmap -R -sP $IP_RANGE | grep -v "Host" | tr '\n' ' ' | sed 's|Nmap|\nNmap|g' | egrep '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}'  | cut -d " " -f5,8-15 | awk '{gsub("Address: ", "");print}';
 }
 
+# Whitelists that are given as arguments to this function are compared, and the FIRST whitelist given is added to based on the differences between it and the second whitelist given.
 _compare_wls () {
 	comm -13 $1 $2 > $WD/weird.txt;
 	cat $WD/weird.txt >> $1 && rm -f $WD/weird.txt;
 	sort $1 > $WD/tmp && mv $WD/tmp $1;
 }
 
+# Scans are run repeatedly to help make the end result more accurate. Some elusive devices evade the scans occasionally.
 for count in 1 2 3
 do
 	_scan_and_parse | sort> $WD/wl_big.$count.txt;
@@ -36,6 +42,7 @@ do
 	fi
 done
 
+# Here, the final whitelist is either created or extended.
 if [ -e ~/FINAL_WHITELIST.txt ]; then
 	_compare_wls ~/FINAL_WHITELIST.txt $WD/whitelist.txt;
 else

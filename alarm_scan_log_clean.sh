a297faa04cf49c48e357273fda0c112c81ec4eed
Joseph Lignelli <36961885+almostheking@users.noreply.github.com>
2018-11-21 13:50:15 -0500

    Update alarm_scan_log_clean.sh
    made simpler

--- alarm_scan_log_clean.sh
+++ alarm_scan_log_clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+set -e
+set -o pipefail
 readonly WD=/home/scanman/alarm_scan_log
 
-if [ -e $WD/alarm*.txt ]; then
-    tar cfv $WD/"alarms_$(date | awk '{print $2,$3,$6}').tar" $WD/alarm*.txt && rm -f $WD/alarm*.txt;
-else
-    exit;
-fi
+tar cfv $WD/"alarms_$(date | awk '{print $2,$3,$6}').tar" $WD/alarm*.txt && rm -f $WD/alarm_scan*.txt;
+
+exit;
